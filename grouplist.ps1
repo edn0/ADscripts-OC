f62ca2aab1779a12ea5b030cf239768dbf017356
edn0 <corentin.edno.blondiau@gmail.com>
2021-04-09 11:28:53 +0200

    Ajout demande usrname + cmd qui va sortir les groupes

--- grouplist.ps1
+++ grouplist.ps1
@@ -1,1 +1,7 @@
 # Ce script à pour but de lister les groupes dont fait partie un utilisateur.
+
+# Cette commande va demander à l'utilisateur le nom de la personne dont il souhaite connaitre les groupes
+$nom = Read-host "Entrez le nom de l'utilisateur"
+
+# Cette commande va prendre le nom donné par l'utilisateur et chercher les groupes dont il est membre
+Get-ADuser $nom -properties memberof
