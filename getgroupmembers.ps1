6930fbb0c8fe07e6fc72898423fff1de3a59725d
edn0 <corentin.edno.blondiau@gmail.com>
2021-04-09 13:55:50 +0200

    v0.1 lister les membres d'un groupe

---
+++ getgroupmembers.ps1
@@ -0,0 +1,7 @@
+# Ce script à pour but de lister les utilisateurs d'un groupe
+
+# Cette commande va permettre d'obtenir aurpès de l'utilisateur le nom du groupe dont il souhaite connaitre les membres
+$group = read-host "Entrez le nom du groupe dont vous souhaitez connaitre les utilisateurs"
+
+# Cette commande va chercher les membres du groupe donné
+Get-ADgroupmember $group > utilisateurs$group
