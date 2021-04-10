0b4b0ec621f3b57b430f81e0932a6166ca655fba
edn0 <corentin.edno.blondiau@gmail.com>
2021-04-10 11:03:28 +0200

    Demande que nom prenom et calcule le reste

--- ADNewUser.ps1
+++ ADNewUser.ps1
@@ -11,11 +11,12 @@ Import-Module ActiveDirectory
 
 # Les commandes suivantes vont recueillir les informations essentielles à la création d'un nouvel utilisateur, puis les stocker dans des valeurs
 
-# Demande le nom de l'utilisateur
-$nom = Read-Host "Entrez le nom et prénom de l'utilisateur"
+# Demande le nom et prénom de l'utilisateur
+$prenom = Read-host "Entrez le prénom de l'utilisateur"
+$nom = Read-Host "Entrez le nom de l'utilisateur"
 
-# Puis son identifiant
-$login = Read-Host "Entrez le nom d'utilisateur"
+# Crée son nom d'utilisateur à partir du nom et prénom
+$login =$prenom.substring(0,1)+$nom
 
 # Cette commande va reprendre les informations enregistrée dans les valeurs introduites plus tôt et les intégrer à la commande de création de l'utilisateur.
 # Elle va ensuite créer le compte, et ajouter deux paramètres. L'utilisateur va être autorisé à changer son mot de passe, mais celui donné plus tôt à une validité infinie dans le temps.
