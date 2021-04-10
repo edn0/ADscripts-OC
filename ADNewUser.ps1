9061a1ca8fb957b93e9b5d5eaa03b74682c837c0
edn0 <corentin.edno.blondiau@gmail.com>
2021-04-10 11:24:33 +0200

    Correction nom

--- ADNewUser.ps1
+++ ADNewUser.ps1
@@ -24,7 +24,7 @@ $group = read-host "Dans quel groupe souhaitez vous insérer le nouvel utilisate
 # Cette commande va reprendre les informations enregistrée dans les valeurs introduites plus tôt et les intégrer à la commande de création de l'utilisateur.
 # Elle va ensuite créer le compte, et ajouter deux paramètres. L'utilisateur va être autorisé à changer son mot de passe, mais celui donné plus tôt à une validité infinie dans le temps.
 
-New-ADUser -Name $nom+$prenom -SamAccountName $login -UserPrincipalName $login@acme.fr -AccountPassword (ConvertTo-SecureString -AsPlainText Bonjour58! -Force) -PasswordNeverExpires $true -CannotChangePassword $false -Enabled $true
+New-ADUser -Name $prenom $nom -SamAccountName $login -UserPrincipalName $login@acme.fr -AccountPassword (ConvertTo-SecureString -AsPlainText Bonjour58! -Force) -PasswordNeverExpires $true -CannotChangePassword $false -Enabled $true
 
 # Cette commande va ajouter l'utilisateur au groupe renseigné
 Add-ADgroupmember -identity $group -members $login
