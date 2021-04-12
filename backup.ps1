417d7b2469eff5c5f1813fcfd983aef44112814b
edno <corentin.edno.blondiau@gmail.com>
2021-04-12 11:34:19 +0200

    user folder backup to fileshare
    does $env:username work ?

---
+++ backup.ps1
@@ -0,0 +1,5 @@
+# Ce script à pour but de faire une sauvegarde quotidienne des données de chaque poste client dans un dossier partagé sur le serveur
+# version 0.1
+# Auteur : Corentin Blondiau
+
+Copy-item -path C:\Users\$env:UserName\Documents\ -Destination \\WYM\Sauvegarde\ -recurse
