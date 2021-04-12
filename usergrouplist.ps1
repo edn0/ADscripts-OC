4aeb4e688132ddd436c2cfdce5f8cdccb5bc4ef9
edno <corentin.edno.blondiau@gmail.com>
2021-04-12 11:17:28 +0200

    V 1.0 OK

--- usergrouplist.ps1
+++ usergrouplist.ps1
@@ -1,9 +1,9 @@
-# Ce script Ã  pour but de lister les groupes dont fait partie un utilisateur.
+# Ce script à pour but de lister les groupes dont fait partie un utilisateur.
 
-# Cette commande va demander Ã  l'utilisateur le nom de la personne dont il souhaite connaitre les groupes
+# Cette commande va demander à l'utilisateur le nom de la personne dont il souhaite connaitre les groupes
 $nom = Read-host "Entrez le nom de l'utilisateur"
 
-# Cette commande va prendre le nom donnÃ© par l'utilisateur et chercher les groupes dont il est membre
+# Cette commande va prendre le nom donné par l'utilisateur et chercher les groupes dont il est membre
 dsquery user -samid $nom | dsget user -memberof | dsget group -samid > groupes$nom.txt
 
 Read-Host "L'export s'est terminé sans encombres"
