02ced908aaacb45cc682c2d82a69c8bf47d81d40
edn0 <corentin.edno.blondiau@gmail.com>
2021-04-14 09:29:02 +0200

    Sauvegarde à terminer

--- backup.ps1
+++ backup.ps1
@@ -1,5 +1,5 @@
 # Ce script à pour but de faire une sauvegarde quotidienne des données de chaque poste client dans un dossier partagé sur le serveur
-# version 0.1
+# version 0.2
 # Auteur : Corentin Blondiau
 
 # Cette commande va faire une copie complète des documents de tout les dossiers utilisateur vers le serveur
