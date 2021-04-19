bb0f59ae20c3be5422a65d824107c5c66f69a21b
edn0 <corentin.edno.blondiau@gmail.com>
2021-04-19 15:46:44 +0200

    Possibilité d'appeler le script avec arguments

---
+++ ADNewUserARG.ps1
@@ -0,0 +1,37 @@
+## Ce script a pour but de tester la possibilité d'appeler ADNewUser.ps1 avec des arguments. Il est voué à disparaitre et a voir ses fonctions intégrées à ADNewUser.ps1
+
+# Ceci est un script ayant pour but de créer des utilisateurs Active Directory à partir des informations qui lui seront données.
+# Le script peut prendre les informations en ligne de commande, ou de manière interactive.
+# Le script va créer un utilisateur avec le prénom et nom complet, et son nom d'utilisateur sera la première lettre de son prénom suivi de son nom de famille complet.
+# version 1.0
+# Auteur : Corentin Blondiau
+
+# Ceci va permettre d'assigner une valeur aux variables nom et prénom directement en appelant le script. Ces paramètres sont obligatoires.
+# Usage : ./ADNewUser.ps1 -prenom Bill -nom Boquet -group Inventions révolutionnaires
+param($prenom, $nom, $group)
+
+# Les commandes suivantes vont recueillir les informations essentielles à la création d'un nouvel utilisateur, puis les stocker dans des valeurs
+
+# Demande le nom et prénom de l'utilisateur
+$prenom = Read-host "Entrez le prénom de l'utilisateur"
+$nom = Read-Host "Entrez le nom de l'utilisateur"
+
+# Crée son nom d'utilisateur à partir du nom et prénom en ne conservant que la première lettre du prénom, associé au nom de famille
+$login = $prenom.substring(0,1)+$nom
+
+# Demande dans quel groupe l'utilisateur doit-il être inséré
+$group = read-host "Dans quel groupe souhaitez vous insérer le nouvel utilisateur ?"
+
+# Cette commande va reprendre les informations enregistrée dans les valeurs introduites plus tôt et les intégrer à la commande de création de l'utilisateur.
+# Elle va ensuite créer le compte, et ajouter deux paramètres. L'utilisateur va être autorisé à changer son mot de passe, mais celui donné plus tôt à une validité infinie dans le temps.
+
+New-ADUser -Name $prenom$nom -SamAccountName $login -UserPrincipalName $login@acme.fr -AccountPassword (ConvertTo-SecureString -AsPlainText Bonjour58! -Force) -PasswordNeverExpires $true -CannotChangePassword $false -Enabled $true
+
+# Cette commande va ajouter l'utilisateur au groupe renseigné
+Add-ADgroupmember -identity $group -members $login
+
+# Confirmation de l'inscription de l'utilisateur
+Write-Host "L'utilisateur $login fait désormais partie de votre domaine et du groupe $group. Un dossier à son nom a été crée dans le dossier Partage."
+
+# Cette commande va permettre de créer un dossier au nom de l'utilisateur dans un dossier partagé
+New-Item -Path C:\Partage\$login -ItemType Directory
