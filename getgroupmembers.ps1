01b8001cc13b5e8bcaee30788e42f387284c479f
edn0 <corentin.edno.blondiau@gmail.com>
2021-04-23 13:44:08 +0200

    caractères spéciaux

--- getgroupmembers.ps1
+++ getgroupmembers.ps1
@@ -10,10 +10,10 @@ function check {
 }
 }
 
-# Cette commande va permettre d'obtenir auprès de l'utilisateur le nom du groupe dont il souhaite connaitre les membres
+# Cette commande va permettre d'obtenir auprÃ¨s de l'utilisateur le nom du groupe dont il souhaite connaitre les membres
 $group = read-host "Entrez le nom du groupe dont vous souhaitez connaitre les utilisateurs"
 
-# Cette commande va chercher les membres du groupe donné
+# Cette commande va chercher les membres du groupe donnÃ©
 Get-ADgroupmember $group | select name > utilisateurs$group.txt
 
-Read-Host "L'export des membres du groupe $group est terminé."
+Read-Host "L'export des membres du groupe $group est terminÃ©."
