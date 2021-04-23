3b2e1879d0fd696d6e1d74cb0f2ed8824340133e
edn0 <corentin.edno.blondiau@gmail.com>
2021-04-23 13:42:11 +0200

    ajout fonction en cours

--- getgroupmembers.ps1
+++ getgroupmembers.ps1
@@ -2,10 +2,18 @@
 # version 1.0
 # Auteur : Corentin Blondiau
 
+param([string]$group)
+
+function check {
+    if ($group -eq ""){
+    $group = Read-host "Entrez le nom du groupe dont vous souhaitez connaitre les membres"
+}
+}
+
 # Cette commande va permettre d'obtenir aurpès de l'utilisateur le nom du groupe dont il souhaite connaitre les membres
 $group = read-host "Entrez le nom du groupe dont vous souhaitez connaitre les utilisateurs"
 
 # Cette commande va chercher les membres du groupe donné
-Get-ADgroupmember $group > utilisateurs$group.txt
+Get-ADgroupmember $group | select name > utilisateurs$group.txt
 
 Read-Host "L'export des membres du groupe $group est terminee."
