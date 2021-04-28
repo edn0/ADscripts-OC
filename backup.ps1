4cb89546cec3c682347fdecc9d4746c45e339e45
edn0 <corentin.edno.blondiau@gmail.com>
2021-04-28 21:38:49 +0200

    changement obtention noms dossiers

--- backup.ps1
+++ backup.ps1
@@ -1,17 +1,19 @@
-# Ce script à pour but de faire une sauvegarde quotidienne des données de chaque poste client dans un dossier partagé sur le serveur
-# Le dossier contenant les vidéos ne sera pas backup par soucis de stockage
-# version 1.0
+# Ce script à pour but de faire une sauvegarde quotidienne des donn�es de chaque poste client dans un dossier partag� sur le serveur
+# Le dossier contenant les vid�os ne sera pas backup par soucis de stockage
+# version 1.2
 # Auteur : Corentin Blondiau
 
-# Cette commande va récupérer les noms d'utilisateurs dans un fichier texte afin d'avoir tout les noms à disposition.
-## sugg : ajouter une commande pour pull de nouveau les username, l'exporter dans ce même usr.txt afin de toujours avoir les noms à jour
-foreach ($username in Get-Content \\WVM\Partage\usr.txt){
+$username = Get-ChildItem C:\Users
+$username | ForEach-Object{
+write $_.name
+}
 
-# Cette commande va définir le chemin du dossier à copier, puis sa destination
+
+
+# Cette commande va d�finir le chemin du dossier à copier, puis sa destination
 $src = "C:\Users\" + $username + "\Documents\"
 $Destination = "\\WVM\SAV\$username\"
 
-# Cette commande va faire une copie complête des documents de tout les dossiers utilisateur vers le serveur.
-# Il est nécessaire d'exclure desktop.ini avec /XF pour que la copie des dossiers système tels que Documents soit bien enregistré au nom de l'utilisateur dans la destination. Autrement le dossier était appelé "Documents". Problème connu de robocopy lorsque l'on fait une copie des dossiers système utilisateur.
+# Cette commande va faire une copie compl�te des documents de tout les dossiers utilisateur vers le serveur.
+# Il est n�cessaire d'exclure desktop.ini avec /XF pour que la copie des dossiers syst�me tels que Documents soit bien enregistr� au nom de l'utilisateur dans la destination. Autrement le dossier �tait appel� "Documents". Probl�me connu de robocopy lorsque l'on fait une copie des dossiers syst�me utilisateur.
 robocopy $src $Destination /MIR /w:0 /r:0 /XF desktop.ini
-}
