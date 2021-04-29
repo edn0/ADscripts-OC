ffa1b200b0b363de0c6c1530fd35ee008218520a
edn0 <corentin.edno.blondiau@gmail.com>
2021-04-29 09:57:40 +0200

    Ajout paramètres et export

--- usergrouplist.ps1
+++ usergrouplist.ps1
@@ -1,12 +1,9 @@
 # Ce script à pour but de lister les groupes dont fait partie un utilisateur.
 
 # Création du paramètre nom et je le rends obligatoire
-param([Parameter(mandatory=$true)][string]$nom)
-
-# Cette commande va demander à l'utilisateur le nom de la personne dont il souhaite connaitre les groupes
-$nom = Read-host "Entrez le nom de l'utilisateur"
+param([Parameter(mandatory=$true)][string]$nomutilisateur)
 
 # Cette commande va prendre le nom donné par l'utilisateur et chercher les groupes dont il est membre
-dsquery user -samid $nom | dsget user -memberof | dsget group -samid > groupes$nom.txt
+dsquery user -samid $nomutilisateur | dsget user -memberof | dsget group -samid > groupes$nomutilisateur.txt
 
 Read-Host "L'export s'est terminé sans encombres"
