5e9c1b09a860cd07a0f8db35f48388b8bab27a37
edn0 <corentin.edno.blondiau@gmail.com>
2021-04-29 09:08:09 +0200

    Ajout commentaires et format

--- backup.ps1
+++ backup.ps1
@@ -1,19 +1,27 @@
-# Ce script à pour but de faire une sauvegarde quotidienne des donn�es de chaque poste client dans un dossier partag� sur le serveur
-# Le dossier contenant les vid�os ne sera pas backup par soucis de stockage
-# version 1.2
+# Ce script à pour but de faire une sauvegarde quotidienne du dossier Doucments de chaque utilisateur s'étant connecté sur le poste
+# version 1.3
 # Auteur : Corentin Blondiau
 
-$username = Get-ChildItem C:\Users
-$username | ForEach-Object{
-write $_.name
+# Cette commande va lister les dossiers dans le dossier Utilisateurs
+$dir = Get-ChildItem C:\Users
+
+# Ici je récupère le nom de chaque dossier avec une confirmation visuelle via Write-Host
+$dir | ForEach-Object{
+Write-Host "ok"
+write $_.FullName
+
+# Cette commande va définir le chemin du dossier à copier, puis sa destination. Je vais prendre le nom de la machine pour éviter d'écraser les données dans le cas où l'utilisateur s'est servi de plusieurs postes.
+$src = $_.FullName + "\Documents"
+$username = $_.name
+$machine = $env:COMPUTERNAME
+$Destination = "\\WVM\Sauvegardes\$username\$machine"
+
+# Je teste si le chemin existe, si non, je vais forcer sa création
+if(!(test-path $destination)){
+New-Item -item-type directory -force -path $destination
 }
 
-
-
-# Cette commande va d�finir le chemin du dossier à copier, puis sa destination
-$src = "C:\Users\" + $username + "\Documents\"
-$Destination = "\\WVM\SAV\$username\"
-
-# Cette commande va faire une copie compl�te des documents de tout les dossiers utilisateur vers le serveur.
-# Il est n�cessaire d'exclure desktop.ini avec /XF pour que la copie des dossiers syst�me tels que Documents soit bien enregistr� au nom de l'utilisateur dans la destination. Autrement le dossier �tait appel� "Documents". Probl�me connu de robocopy lorsque l'on fait une copie des dossiers syst�me utilisateur.
-robocopy $src $Destination /MIR /w:0 /r:0 /XF desktop.ini
+# Cette commande va faire une copie complête des documents de tout les dossiers utilisateur vers le serveur.
+# Il est nécessaire d'exclure desktop.ini avec /XF pour que la copie du dossier Documents soit bien enregistrée au nom de l'utilisateur dans la destination. Autrement le dossier était appelé "Documents" avec l'icône du dossier personnel Documents. Problème connu de robocopy lorsque l'on fait une copie des dossiers personnels utilisateur.
+robocopy $src $Destination -force /MIR /w:0 /r:0 /XF desktop.ini
+}
