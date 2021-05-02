85fac7a3dab33c4c6daf60886d3802a8c17e1ad2
edn0 <corentin.edno.blondiau@gmail.com>
2021-05-02 11:54:31 +0200

    Ajout des règles de permissions

--- ADNewUser.ps1
+++ ADNewUser.ps1
@@ -1,7 +1,7 @@
 # Ceci est un script a pour but de créer des utilisateurs Active Directory à partir des informations qui lui seront données.
 # Le script peut prendre les informations en ligne de commande, ou de manière interactive.
 # Le script va créer un utilisateur avec le prénom et nom complet, et son nom d'utilisateur sera la première lettre de son prénom suivi de son nom de famille complet.
-# version 1.2
+# version 1.3
 # Auteur : Corentin Blondiau
 
 # Ceci va permettre d'assigner une valeur aux variables nom et prénom directement en appelant le script. Ces paramètres sont obligatoires.
@@ -18,8 +18,16 @@ New-ADUser -Name $prenom$nom -SamAccountName $login -UserPrincipalName $login@ac
 # Cette commande va ajouter l'utilisateur au groupe renseigné
 Add-ADgroupmember -identity $group -members $login
 
-# Confirmation de l'inscription de l'utilisateur
-Write-Host "L'utilisateur $login fait désormais partie de votre domaine et du groupe $group. Un dossier à son nom a été créé dans le dossier \\WVM\Partage\Personnel."
-
 # Cette commande va permettre de créer un dossier au nom de l'utilisateur dans un dossier partagé
 New-Item -Path C:\Partage\Personnel\$login -ItemType Directory
+
+# Définition des règles de permission
+$usraccess = New-Object System.Security.AccessControl.FileSystemAccessRule("$login","FullControl","ContainerInherit, ObjectInherit", "None","Allow")
+$adminaccess = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrateur","FullControl","ContainerInherit, ObjectInherit", "None","Allow")
+# Ajout des droits de contrôle total pour l'utilisateur et l'administrateur
+$acl.SetAccessRule($usraccess)
+$acl.SetAccessRule($adminaccess)
+$acl | Set-Acl
+
+# Confirmation de l'inscription de l'utilisateur
+Write-Host "L'utilisateur $login fait désormais partie de votre domaine et du groupe $group. Un dossier à son nom a été créé dans le dossier \\WVM\Partage\Personnel."
